65cd6d2d04f778ec5d8396f96212f60b89d4ace9
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-07 00:56:42 -0400

    comit

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ window.addEventListener("DOMContentLoaded", () => {
   const people = document.getElementById("people");
   const tipBoxes = document.querySelectorAll(".tip-box");
   const enterElements = [bill, people];
+  const tipPerPerson = document.getElementById("tip-per-person");
+  const totalTip = document.getElementById("total-tip");
+  const resetButton = document.getElementById("reset");
 
   //functions
+  function calculateTips() {
+    let bill = enterElements[0];
+    let tipBoxArr = [...tipBoxes];
+    // iteratoes through tipBoxes to find selected box
+    let found = tipBoxArr.find((item) => {
+      return item.classList.contains("selected");
+    });
+    // tip percent is inputBox or selectBox value
+    let tipPercent =
+      found.value || parseFloat(found.innerText.replace("%", ""));
+
+    let tipWhole = bill.value * (tipPercent / 100);
+    let tipEach = tipWhole / people.value;
+    console.log(tipEach);
+    tipPerPerson.lastChild.previousElementSibling.innerText = tipEach;
+    totalTip.lastChild.previousElementSibling.innerText = tipWhole;
+  }
   //removes selected class from each tipBox
   function removeSelected() {
     tipBoxes.forEach((box) => {
       box.classList.remove("selected");
     });
+    tipBoxes[5].value = 0;
   }
   // adds selected classList
   function checked(event) {
@@ -20,18 +41,31 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function checkInputEmpty() {
     enterElements.forEach((item) => {
+      //remove borders and set tooltip to hidden
       item.classList.remove("orange-border");
       item.previousElementSibling.setAttribute("hidden", true);
       if (item.value == 0) {
+        // if input empty add orange border and display tooltip
         item.classList.add("orange-border");
-
         item.previousElementSibling.removeAttribute("hidden");
       }
     });
+    // check if all input fields are filled
     let inputsFilled = enterElements.every((item) => {
       return item.value > 0;
     });
-    console.log(inputsFilled);
+
+    if (inputsFilled) {
+      calculateTips();
+    }
+  }
+
+  function reset() {
+    bill.value = 0;
+    people.value = 0;
+
+    tipPerPerson.lastChild.previousElementSibling.innerText = 0;
+    totalTip.lastChild.previousElementSibling.innerText = 0;
   }
 
   // event listeners
@@ -47,4 +81,6 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  resetButton.addEventListener("click", reset);
 });
